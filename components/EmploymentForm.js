bed6c5372240d61e7a83c01f6fa97290dcc984f8
swalla50 <stevenwallacejr@aol.com>
2022-09-27 22:50:44 -0400

    Update to Employment form.
    updates to employment/ build renew

--- components/EmploymentForm.js
+++ components/EmploymentForm.js
@@ -1,107 +1,109 @@
 import Button from '@material-ui/core/Button';
-import {React,useState} from 'react';
+import { React, useState } from 'react';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import emailjs from 'emailjs-com';
 import { Divider } from '@material-ui/core';
 import '../styles/EmploymentForm.module.css';
 
-function sendEmail(e){
+function sendEmail(e) {
     e.preventDefault();
 
-emailjs.sendForm('service_r9b69jg', 'template_ifl9jsv', e.target, 'user_OIuQ0ZFNNH6wSdbZCzQet')
-  .then((result) => {
-      console.log(result.text);
-  }, (error) => {
-      console.log(error.text);
-  });
+    emailjs.sendForm('service_0vpivnq', 'template_ifl9jsv', 'form#form-contact', 'user_OIuQ0ZFNNH6wSdbZCzQet')
+        .then((result) => {
+            console.log(result.text);
+        }, (error) => {
+            console.log(error.text);
+        });
 }
 
- export default function EmploymentForm(){
+export default function EmploymentForm() {
 
 
     const [email, setEmail] = useState("");
-    const [ss, setSS] = useState("");
-    const [dob, setDOB] = useState("");
-    const [city, setCity] = useState("");
-    const [state,setState] = useState("");
-    const [zip, setZip] = useState("");
-    const [address, setAddress] = useState("");
+    // const [ss, setSS] = useState("");
+    // const [dob, setDOB] = useState("");
+    // const [city, setCity] = useState("");
+    // const [state,setState] = useState("");
+    // const [zip, setZip] = useState("");
+    // const [address, setAddress] = useState("");
     const [name, setName] = useState("");
     const [phone, setPhone] = useState("");
     const [subject, setSubject] = useState("");
     const [message, setMessage] = useState("");
-    const [ED1, setED1] = useState("");
-    const [ED2, setED2] = useState("");
-    const [ED3, setED3] = useState("");
-    const [Grad1, setGrad1] = useState("");
-    const [Grad2, setGrad2] = useState("");
-    const [schoolInfo, setschoolInfo] = useState("");
-    const [reference1, setreference1] = useState("");
-    const [reference2, setreference2] = useState("");
-    const [reference3, setreference3] = useState("");
-    const [reference4, setreference4] = useState("");
-    const [reference5, setreference5] = useState("");
-    const [general1, setgeneral1] = useState("");
-    const [general2, setgeneral2] = useState("");
-    const [general3, setgeneral3] = useState("");
-    const [general4, setgeneral4] = useState("");
-    const [general5, setgeneral5] = useState("");
-    const [general6, setgeneral6] = useState("");
-    const [general7, setgeneral7] = useState("");
-    const [general8, setgeneral8] = useState("");
-    const [general9, setgeneral9] = useState("");
+    const [file, setFile] = useState("");
+    // const [ED1, setED1] = useState("");
+    // const [ED2, setED2] = useState("");
+    // const [ED3, setED3] = useState("");
+    // const [Grad1, setGrad1] = useState("");
+    // const [Grad2, setGrad2] = useState("");
+    // const [schoolInfo, setschoolInfo] = useState("");
+    // const [reference1, setreference1] = useState("");
+    // const [reference2, setreference2] = useState("");
+    // const [reference3, setreference3] = useState("");
+    // const [reference4, setreference4] = useState("");
+    // const [reference5, setreference5] = useState("");
+    // const [general1, setgeneral1] = useState("");
+    // const [general2, setgeneral2] = useState("");
+    // const [general3, setgeneral3] = useState("");
+    // const [general4, setgeneral4] = useState("");
+    // const [general5, setgeneral5] = useState("");
+    // const [general6, setgeneral6] = useState("");
+    // const [general7, setgeneral7] = useState("");
+    // const [general8, setgeneral8] = useState("");
+    // const [general9, setgeneral9] = useState("");
 
 
     function handleSubmit(e) {
         e.preventDefault();
-        console.log(email, username, phone, subject, message);
+        console.log(email, phone, subject, message);
         // clearing the values
         setEmail("");
         setName("");
-        setPhone("");
+        // setPhone("");
         setSubject("");
         setMessage("");
-        setCity("");
-        setState("");
-        setZip("");
-        setAddress("");
-        setDOB("");
-        setSS("");
-        setED1("");
-        setED2("");
-        setED3("");
-        setGrad1("");
-        setGrad2("");
-        setschoolInfo("");
-        setreference1("");
-        setreference2("");
-        setreference3("");
-        setreference4("");
-        setreference5("");
-        setgeneral1("");
-        setgeneral2("");
-        setgeneral3("");
-        setgeneral4("");
-        setgeneral5("");
-        setgeneral6("");
-        setgeneral7("");
-        setgeneral8("");
-        setgeneral9("");
+        setFile("");
+        // setCity("");
+        // setState("");
+        // setZip("");
+        // setAddress("");
+        // setDOB("");
+        // setSS("");
+        // setED1("");
+        // setED2("");
+        // setED3("");
+        // setGrad1("");
+        // setGrad2("");
+        // setschoolInfo("");
+        // setreference1("");
+        // setreference2("");
+        // setreference3("");
+        // setreference4("");
+        // setreference5("");
+        // setgeneral1("");
+        // setgeneral2("");
+        // setgeneral3("");
+        // setgeneral4("");
+        // setgeneral5("");
+        // setgeneral6("");
+        // setgeneral7("");
+        // setgeneral8("");
+        // setgeneral9("");
     }
-    return(
-    <Card>
-        <CardContent style={{paddingBottom:'55px'}}>
-            <div className= 'contact-form-container' style={{paddingInline:'5%'}}>
-                <form id='form-contact' onSubmit={sendEmail} style={{background: 'white', borderRadius:'20px', padding:'10px', boxShadow: '0 32px 20px rgb(0 0 0 / 26%)'}}>
-                    <div className='form-group'>
-                        <div className= 'contactrow'>
-                            <h2>Online Job Application</h2>
-                            <div className='col-md-6' style={{paddingBottom:'3%'}}>
-                                <input placeholder='Name' onChange={(e) => setName(e.target.value)} id ='name' type='text' className='form-control' name='name' value = {name}
-                                style={{borderLeft:'none',borderRight:'none', borderTop: 'none', borderBottom:'solid #6685bb', padding:'0px'}} />
-                            </div>
-                            <div className='form-group' onChange={(e) => setAddress(e.target.value)} style={{paddingBottom:'3%'}}>
+    return (
+        <Card>
+            <CardContent style={{ paddingBottom: '55px' }}>
+                <div className='contact-form-container' style={{ paddingInline: '5%' }}>
+                    <form enctype="multipart/form-data" method="post" id='form-contact' onSubmit={sendEmail} style={{ background: 'white', borderRadius: '20px', padding: '10px', boxShadow: '0 32px 20px rgb(0 0 0 / 26%)' }}>
+                        <div className='form-group'>
+                            <div className='contactrow' style={{ textAlign: 'center' }}>
+                                <h2>Come Join the CLS Medical Team!</h2>
+                                <div className='col-md-6' style={{ paddingBottom: '3%' }}>
+                                    <input placeholder='Name' onChange={(e) => setName(e.target.value)} id='name' type='text' className='form-control' name='name' value={name}
+                                        style={{ borderLeft: 'none', borderRight: 'none', borderTop: 'none', borderBottom: 'solid #6685bb', padding: '0px', width: '50%' }} />
+                                </div>
+                                {/* <div className='form-group' onChange={(e) => setAddress(e.target.value)} style={{paddingBottom:'3%'}}>
                                 <input placeholder='Street Address' id ='address' type='text' className='form-control' name='address' value ={address}
                                 style={{borderLeft:'none',borderRight:'none', borderTop: 'none', borderBottom:'solid #6685bb', width:'50%'}} />
                             </div>
@@ -118,24 +120,24 @@ emailjs.sendForm('service_r9b69jg', 'template_ifl9jsv', e.target, 'user_OIuQ0ZFN
                                     <input placeholder='Zip Code' onChange={(e) => setZip(e.target.value)} id ='zip' type='text' className='form-control' name='zip' value = {zip}
                                     aria-describedby='emailHelp'   style={{borderLeft:'none',borderRight:'none', borderTop: 'none', borderBottom:'solid #6685bb'}} />
                                 </div>
-                            </div>
-                        
-                            <div className='address-group'>
-                                <div className='form-group' style={{paddingBottom:'3%'}}>
-                                    <input placeholder='Email' onChange={(e) => setEmail(e.target.value)} id ='email' type='email' className='form-control' rows='1' name='email' value = {email}
-                                    style={{padding:'0px',borderLeft:'none',borderRight:'none', borderTop: 'none', borderBottom:'solid #6685bb'}} />
-                                </div>
-                                <div className='col-md-6' style={{paddingBottom:'3%'}}>
+                            </div> */}
+
+                                <div className='address-group'>
+                                    <div className='form-group' style={{ paddingBottom: '3%' }}>
+                                        <input placeholder='Email' onChange={(e) => setEmail(e.target.value)} id='email' type='email' className='form-control' rows='1' name='email' value={email}
+                                            style={{ padding: '0px', borderLeft: 'none', borderRight: 'none', borderTop: 'none', borderBottom: 'solid #6685bb', width: '50%' }} />
+                                    </div>
+                                    {/* <div className='col-md-6' style={{paddingBottom:'3%'}}>
                                     <input placeholder='Date of Birth' onChange={(e) => setDOB(e.target.value)} id ='dob' type='text' className='form-control' name='dob' value = {dob}
                                     style={{borderLeft:'none',borderRight:'none', borderTop: 'none', borderBottom:'solid #6685bb'}} />
                                 </div>
                                 <div className='col-md-6' style={{paddingBottom:'3%'}}>
                                     <input placeholder='Social Security' onChange={(e) => setSS(e.target.value)} id ='zip' type='text' className='form-control' name='ss' value = {ss}
                                     style={{borderLeft:'none',borderRight:'none', borderTop: 'none', borderBottom:'solid #6685bb'}} />
+                                </div> */}
                                 </div>
-                            </div>
-                            <Divider style={{height:'4px', backgroundColor:'black'}}></Divider>
-                            <h2>Education</h2>
+                                <Divider style={{ height: '4px', backgroundColor: 'black' }}></Divider>
+                                {/* <h2>Education</h2>
                             <div className='education-group1'style={{display:'inline-flex'}}>
                                 <p>Highest Level of Education:</p><t></t>
                                 <div className='checkbox1' style={{paddingInline:'10px'}}>
@@ -192,8 +194,8 @@ emailjs.sendForm('service_r9b69jg', 'template_ifl9jsv', e.target, 'user_OIuQ0ZFN
                                 <div className='reference' style={{paddingInline:'10px'}}>
                                     <input type="text" onChange={(e) => setreference3(e.target.value)} id="reference3" name="reference3" style={{paddingBottom:'70px', width:'50%', border: 'solid rgb(102, 133, 187)'}} value={reference3} />
                                 </div>
-                            </div><br></br>
-                            <div className='refence-group'style={{}}>
+                            </div><br></br> */}
+                                {/* <div className='refence-group'style={{}}>
                                 <p>Reference #4:</p><t></t>
                                 <div className='reference' style={{paddingInline:'10px'}}>
                                     <input type="text" onChange={(e) => setreference4(e.target.value)} id="reference4" name="reference4" style={{paddingBottom:'70px', width:'50%', border: 'solid rgb(102, 133, 187)'}} value={reference4} />
@@ -317,24 +319,27 @@ emailjs.sendForm('service_r9b69jg', 'template_ifl9jsv', e.target, 'user_OIuQ0ZFN
                                     <label htmlFor="general2">No</label>
                                 </div>
                             </div><br></br>
-                    
-                            <Divider style={{height:'4px', backgroundColor:'black'}}></Divider>
+                
+                            <Divider style={{height:'4px', backgroundColor:'black'}}></Divider> */}
 
-                            <div className='upload'style={{}}>
-                                <p>Upload Resume:</p><t></t>
-                                <div className='resume' style={{paddingInline:'10px', borderRadius:"20px"}}>
-                                    <input type="file" id="resumeFile" name="filename" style={{ border: 'solid rgb(102, 133, 187)', borderRadius:"20px", cursor:"pointer"}} />
+                                <div className='upload' style={{}}>
+                                    <p>Upload Resume:</p><t></t>
+                                    <div className='resume' style={{ paddingInline: '10px', borderRadius: "20px" }}>
+                                        <input type="file" id="filename" name="filename" onChange={(e) => setFile(e.target.value)} style={{ border: 'solid rgb(102, 133, 187)', borderRadius:'4px', cursor: "pointer" }} />
+                                    </div>
+                                </div><br></br>
+
+                                <div className='disc-message' style={{textAlign:'left', fontSize:'11px',fontStyle:'italic'}}>
+                                    <p>All qualified applicants will receive consideration for employment without regard to race, color, religion, sex, sexual orientation, gender identity, national origin, disability, or status as a protected veteran.</p><t></t>
+                                </div><br></br>
+                                <div className='button-row' style={{ width: '100%', textAlign: 'center', paddingTop: '20px'}}>
+                                    <Button variant='outlined' onclick={handleSubmit} value='Send' type='submit' className='primary-btn submit' style={{ width: 'fit-content', fontSize: 'large', border: '2px solid #dc0000', color: '#dc0000' }}>Submit</Button>
                                 </div>
-                            </div><br></br>
-                            
-                            <div className='button-row' style={{width:'100%', textAlign:'center', paddingTop:'20px'}}>
-                                <Button variant='outlined' onClick={handleSubmit} type= 'submit' className='primary-btn submit' style={{width:'fit-content', fontSize:'large', border:'2px solid #dc0000',color: '#dc0000'}}>Submit</Button>
                             </div>
                         </div>
-                    </div>
-                </form>
-            </div>
-        </CardContent>
-    </Card>    
+                    </form>
+                </div>
+            </CardContent>
+        </Card>
     );
 }
